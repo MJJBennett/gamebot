1a5bff54b6986c26b3aeec0eb736409025c4717a
Michael Bennett <michaelbennett.dev@gmail.com>
2019-10-06 23:55:46 -0700

    Several updates, starting to set up framework.

---
+++ include/bot.hpp
@@ -0,0 +1,13 @@
+#ifndef BOT_HPP
+#define BOT_HPP
+
+namespace qb
+{
+class Bot
+{
+public:
+    Bot();
+};
+}
+
+#endif  // BOT_HPP
---
+++ include/web.hpp
@@ -0,0 +1,11 @@
+#ifndef GAMEBOT_WEB_HPP
+#define GAMEBOT_WEB_HPP
+
+#include "nlohmann/json.hpp"
+
+namespace web{
+namespace json = nlohmann::json;
+json get_bot_socket(){}
+}
+
+#endif // GAMEBOT_WEB_HPP
