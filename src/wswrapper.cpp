02ffa778877ba0cc95393f97d267e70b4bf462e7
Michael Bennett <michaelbennett.dev@gmail.com>
2019-10-16 23:00:53 -0700

    Correctly close websocket stream prior to destroying object.

--- src/wswrapper.cpp
+++ src/wswrapper.cpp
@@ -16,24 +16,42 @@ web::WSWrapper::~WSWrapper()
         // Regardless, this works for now.
         return;
     }
-    ws_->close(websocket::close_code::normal);
+    try
+    {
+        ws_->close(websocket::close_code::normal);
+    }
+    catch (const std::exception& e)
+    {
+        qb::log::err("Caught while closing websocket: ", e.what());
+    }
     qb::log::normal("Closed the websocket.");
 }
 
-web::WSWrapper::WSWrapper(boost::asio::io_context& ioc) : ctx_(boost::asio::ssl::context::tlsv12_client), ioc_(ioc), resolver_(ioc)
+web::WSWrapper::WSWrapper(boost::asio::io_context& ioc)
+    : ctx_(boost::asio::ssl::context::tlsv12_client), ioc_(ioc), resolver_(ioc)
 {
     qb::log::point("Constructing websocket wrapper.");
     ctx_.set_options(boost::asio::ssl::context::default_workarounds);
     ctx_.set_verify_mode(boost::asio::ssl::verify_peer);
     qb::log::point("Constructing inner websocket.");
-    ws_ = std::make_unique<web::WebSocket>(ioc_, ctx_);}
+    ws_ = std::make_unique<web::WebSocket>(ioc_, ctx_);
+}
 
-web::WSWrapper::WSWrapper(web::WSWrapper&& wsw) : ws_{std::move(wsw.ws_)}, ctx_{std::move(wsw.ctx_)}, ioc_(wsw.ioc_), resolver_{std::move(wsw.resolver_)}
+web::WSWrapper::WSWrapper(web::WSWrapper&& wsw)
+    : ws_{std::move(wsw.ws_)}, ctx_{std::move(wsw.ctx_)}, ioc_(wsw.ioc_), resolver_{std::move(wsw.resolver_)}
 {
 }
 
 void web::WSWrapper::disconnect()
 {
+    try
+    {
+        ws_->close(websocket::close_code::normal);
+    }
+    catch (const std::exception& e)
+    {
+        qb::log::err("Caught while closing websocket: ", e.what());
+    }
     ws_.reset();
 }
 
