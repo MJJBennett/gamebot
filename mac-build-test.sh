2a6ce11f0cb1e16b1d0118bf310b8e7aa4eb616c
Michael Bennett <michaelbennett.dev@gmail.com>
2019-10-18 22:36:47 -0700

    Get Google Tests functioning.

--- mac-build-test.sh
+++ mac-build-test.sh
@@ -1,9 +1,9 @@
 mkdir output >/dev/null 2>&1
 cd output >/dev/null 2>&1
-cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8
+cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8 -DQBTEST=True
 # rm ../compile_commands.json
 # ln -s compile_commands.json ../compile_commands.json
 cp compile_commands.json ..
 cd ..
 echo "Fixing compile commands..."
-./fix-compile-commands.py include src auth
+./fix-compile-commands.py include src auth test
