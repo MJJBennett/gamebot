0a148822f92f6313cbdb5df080639367ef1a0a01
Michael Bennett <michaelbennett.dev@gmail.com>
2019-10-27 21:30:20 -0700

    Update test to match implementation (hah)

--- test/Test_Parse.cpp
+++ test/Test_Parse.cpp
@@ -7,7 +7,7 @@ using namespace qb::parse;
 
 TEST(Parse, remove_non_cmd)
 {
-    std::string a{"!qb do something01"};
+    std::string a{"!qb do something"};
     std::string b{"<@222222222> !qb do otherwise!"};
     EXPECT_EQ(remove_non_cmd(a), "!qb do something");
     EXPECT_EQ(remove_non_cmd(b), "!qb do otherwise!");
