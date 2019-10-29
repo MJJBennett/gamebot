fb6ece80ed6b13402244fdd5a2abd32576cc1581
Michael Bennett <michaelbennett.dev@gmail.com>
2019-10-29 15:59:02 -0700

    Add config option for emote data.

--- include/config.hpp
+++ include/config.hpp
@@ -6,13 +6,15 @@
 
 namespace qb::config
 {
-using json       = nlohmann::json;
+using json                                = nlohmann::json;
 constexpr int max_skribbl_wordsize        = 30;
 const std::string max_skribble_wordsize_s = "30";
 
 inline json& get_config_()
 {
-    static auto conf_ = json{{"command_start", "!qb "}, {"skribbl_path", "../data/skribbl.json.current"}};
+    static auto conf_ = json{{"command_start", "!qb "},
+                             {"skribbl_path", "../data/skribbl.json.current"},
+                             {"emote_path", "../data/emote.kvs"}};
     return conf_;
 }
 
@@ -24,6 +26,11 @@ inline std::string cmd_start()
 inline std::string skribbl_data_file()
 {
     return get_config_()["skribbl_path"];
+}
+
+inline std::string emote_data_file()
+{
+    return get_config_()["emote_path"];
 }
 
 } // namespace qb::config
